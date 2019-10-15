f18533a598f843f51c41e80c681f321e6dd3e990
liutaochange <liutaochange@163.com>
2019-10-15 18:59:15 +0800

    feat: 增加common state

--- web/src/store/reducer.js
+++ web/src/store/reducer.js
@@ -1,5 +1,19 @@
 import { combineReducers } from 'redux'
+import { UPDATE_LOADING } from './actionType'
 import user from './user/reducer'
+const commonState = {
+  loading: false
+}
+// common
+const common = (state = commonState, { type, payload }) => {
+  switch (type) {
+    case UPDATE_LOADING:
+      return { ...payload }
+    default:
+      return state
+  }
+}
 export default combineReducers({
+  common,
   user
 })
