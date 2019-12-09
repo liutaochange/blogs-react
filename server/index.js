01b47712f5401ef836db21c9583873cd43126ceb
梁留涛 <liangliutao@qiyi.com>
2019-12-09 17:50:40 +0800

    feat: 逻辑梳理

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ const app = new Koa()
 
 // 允许跨域访问, 设置接收携带Authorization的请求
 app.use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', ctx.headers.origin)
-    ctx.set('Access-control-Allow-Methods', '*')
-    ctx.set('Access-control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
-    await next()
+  ctx.set('Access-Control-Allow-Origin', ctx.headers.origin)
+  ctx.set('Access-control-Allow-Methods', '*')
+  ctx.set(
+    'Access-control-Allow-Headers',
+    'X-Requested-With,content-type,Authorization'
+  )
+  await next()
 })
 
 app.use(bodyparser())
-app.use(router.routes()).use(router.allowedMethods())  // 路由配置
+app.use(router.routes()).use(router.allowedMethods()) // 路由配置
 app.listen(4399)
